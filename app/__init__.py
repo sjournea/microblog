305b039fe71e4b0a3f7a843c7baab7ba37b58607
Steve Journeay <sjournea@taralaur.com>
2020-10-06 15:37:05 -0700

    Email Support - Chapter 10

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 from .config import Config
 
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
+mail = Mail(app)
 
 from app import routes, models, errors
 
