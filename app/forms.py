6877baf5b94051dec2b8e13250bed66e422f85a2
Steve Journeay <sjournea@taralaur.com>
2020-10-05 20:13:18 -0700

    Chapter 6

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 
 from .models import User
 
@@ -27,4 +27,9 @@ class RegistrationForm(FlaskForm):
 	def validate_email(self, email):
 		user = User.query.filter_by(email=email.data).first()
 		if user is not None:
-			raise ValidationError('Please use a different email address.')	
+			raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+	submit = SubmitField('Submit')
