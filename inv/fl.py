7e4b87cffe5ba6c4ac67d1a378d5a9b8d312148d
Steve Journeay <sjournea@taralaur.com>
2020-10-07 12:05:00 -0700

    Added flask-babel and texts to translate in python code

--- inv/fl.py
+++ inv/fl.py
@@ -3,16 +3,14 @@ from invoke import task
 
 FLASK = "flask"
 
-@task(
-    help={}
-)
+
+@task(help={})
 def run(ctx):
     """Run the flask application"""
     ctx.run(f"{FLASK} run")
 
-@task(
-    help={}
-)
+
+@task(help={})
 def shell(ctx):
     """Run the flask shell"""
     ctx.run(f"{FLASK} shell")
